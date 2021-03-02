51d50d88046f3dfacc536c54fbf5a2a0793785bb
suraj <surajido111@gmail.com>
2021-03-02 07:39:09 +0545

    modified model

--- Feedback/models.py
+++ Feedback/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Feedback(models.Model):
-    feedback = models.CharField(max_length=200)
+    feedback = models.CharField(max_length=200,null=True, blank=True)
     name = models.CharField(max_length=200,null=True,blank=True)
     created_date = models.DateTimeField(
         default=timezone.now, null=True, blank=True)
